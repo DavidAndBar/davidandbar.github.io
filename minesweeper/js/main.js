d65660420d4646cab958a38144cbb594850e89bb
DavidAndBar <davidbar22@hotmail.com>
2024-12-08 22:18:48 -0500

    tries to fix mobile user experience

--- minesweeper/js/main.js
+++ minesweeper/js/main.js
@@ -241,4 +241,11 @@ $(document).ready(() => {
         });
     });
 
+    $("html").css({
+        "-webkit-user-select": "none",
+        "-moz-user-select": "none",
+        "-ms-user-select": "none",
+        "user-select": "none"
+    });
+
 })
